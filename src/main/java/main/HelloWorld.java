3504177b2a2c1e31ec072ab8ca61d338cb6755a8
PHENOMENONXXI <40502122+PHENOMENONXXI@users.noreply.github.com>
2022-01-20 23:31:01 +0300

    Update and rename src/main/java/example/Main.java to src/main/java/main/HelloWorld.java

---
+++ src/main/java/main/HelloWorld.java
@@ -0,0 +1,13 @@
+package main;
+
+public class HelloWorld {
+    public static final String TEXT = "Hello, World welcome to Jenkins pipeline demo!";
+    public static void main(String[] args) {
+        System.out.println(TEXT);
+
+    }
+
+    public String myMethod() {
+        return TEXT;
+    }
+}
